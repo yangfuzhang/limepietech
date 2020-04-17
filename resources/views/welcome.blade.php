ddacbc4cbded374836cd35a7c5117a226e68638c
yangfuzhang <yangfuzhang0720@126.com>
2020-04-18 06:21:53 +0800

    project updated

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', '酸橙派API') }}</title>
+    <title>{{ config('app.name', '酸橙派') }}</title>
 
     <!-- Styles -->
     <style>
